032f0387b9594668a0374a04609c360bdc40325c
MoAbyari <mo.abyari88@gmail.com>
2021-09-10 09:31:52 +1000

    Created API

--- Server/server.js
+++ Server/server.js
@@ -17,6 +17,42 @@ app.get('/info', (req, res) => {
     });
 });
 
+app.get('/vaccine', (req, res) => {
+    axios.get('https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/agedata_vaccines.json')
+    .then((response) => {
+      console.log(response.data.data)
+        res.json(response.data.data);
+    });
+});
+
+app.get('/cases-by-age', (req, res) => {
+    axios.get('https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/agedata.json')
+    .then((response) => {
+        res.json(response.data.data);
+    });
+});
+
+app.get('/lga-daily-cases', (req, res) => {
+    axios.get('https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/lga_daily_cases.json')
+    .then((response) => {
+        res.json(response.data.data);
+    });
+});
+
+app.get('/fatalities-by-age', (req, res) => {
+    axios.get('https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/datafiles/fatalitiesdata.json')
+    .then((response) => {
+        res.json(response.data.data);
+    });
+});
+
+app.get('/nsw-geojson', (req, res) => {
+    axios.get('https://nswdac-covid-19-postcode-heatmap.azurewebsites.net/geojson/lga_2019_nsw_simplified.json')
+        .then((response) => {
+            res.json(response.data);
+        });
+});
+
 // 404 page:
 app.use((req, res) => {
     res.status(404).send({ url: `${req.originalUrl} not found` });
